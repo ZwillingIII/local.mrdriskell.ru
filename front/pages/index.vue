560dd5006338b4d4c8a2ee6f50ec25f460adb9c5
zwilling <zwill@bokus.ru>
2024-08-23 10:00:15 +0300

    first commit

---
+++ front/pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  button(@click="test") Test
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'IndexPage',
+  methods: {
+    test() {
+      fetch('/api/test')
+        .then(response => console.log(response));
+    }
+  }
+})
+</script>
