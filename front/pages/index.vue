db5e66098201d8ce257278afbe0474644748983e
zwilling <zwill@bokus.ru>
2024-08-27 11:40:30 +0300

    Рабочий докер

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,18 +1,34 @@
 <template lang="pug">
-  button(@click="test") Test
+  div
+    button(@click="test") {{ btnTxt }}
+    div(v-if="errorTxt.length") {{ errorTxt }}
 </template>
 
 <script>
 import Vue from 'vue';
+import axios from "axios";
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      btnTxt: 'Test',
+      errorTxt: ''
+    }
+  },
   methods: {
     test() {
-      fetch('http://localhost/back', {
-        method: 'POST'
-      })
-        .then(response => console.log(response));
+      let data = this;
+
+      data.errorTxt = '';
+
+      axios.post('http://localhost/api/test')
+        .then(function (response) {
+          data.btnTxt = response.data[0];
+        })
+        .catch(function (error) {
+          data.errorTxt = error.message;
+        })
     }
   }
 })
